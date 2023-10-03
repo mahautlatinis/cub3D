ec7c7fa74b4db19f91a1f092c2eff6bdd006d330
malatini <malatini@student.42.fr>
2023-10-03 22:08:38 +0200

    fix pre processing error

--- cub3d/manda/includes/cub3D_defines.h
+++ cub3d/manda/includes/cub3D_defines.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3D_defines.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qdam <qdam@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mahautlatinis <mahautlatinis@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 15:17:42 by qdam              #+#    #+#             */
-/*   Updated: 2021/09/08 16:16:43 by qdam             ###   ########.fr       */
+/*   Updated: 2023/10/03 22:08:29 by mahautlatin      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,22 +25,12 @@
 # define SPEED_MOV	0.1
 # define N_RAYS		400
 
-# ifdef __linux__
-#  define KEY_ESC	65307
-#  define KEY_LEFT	65361
-#  define KEY_RIGHT	65363
-#  define KEY_W		'w'
-#  define KEY_A		'a'
-#  define KEY_S		's'
-#  define KEY_D		'd'
-# else
-#  define KEY_ESC	53
-#  define KEY_LEFT	123
-#  define KEY_RIGHT	124
-#  define KEY_W		13
-#  define KEY_A		0
-#  define KEY_S		1
-#  define KEY_D		2
-# endif
+# define KEY_ESC	53
+# define KEY_LEFT	123
+# define KEY_RIGHT	124
+# define KEY_W		13
+# define KEY_A		0
+# define KEY_S		1
+# define KEY_D		2
 
 #endif
